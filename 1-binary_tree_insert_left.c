584c6ce682d4da646317c638535f248b7a2e19ca
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-03 20:08:55 +0200

    missing blank line after declarations

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,15 +12,16 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = binary_tree_node(parent, value);
-	if (new_node == NULL || parent == NULL)
-		return (NULL);
+	binary_tree_t *new_node;
 
-	if (parent->left != NULL)
-	{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
-	}
+	if (parent == NULL)
+		return (NULL);
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+	new_node->left = parent->left;
 	parent->left = new_node;
+	if (new_node->left)
+		new_node->left->parent = new_node;
 	return (new_node);
 }
