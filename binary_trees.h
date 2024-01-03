4275fed100d2b36ff563a9331a3e71fcf05c2835
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-03 21:08:52 +0200

    2. Insert right

--- binary_trees.h
+++ binary_trees.h
@@ -35,4 +35,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
  * that inserts a node as the left-child of another node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+
+/*
+ * binary_tree_insert_right function
+ * that inserts a node as the right-child of another node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+
 #endif
