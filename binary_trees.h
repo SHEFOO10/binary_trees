cda61cfe0b526f931252bfd1c4a19f3d1d21dd9b
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-04 21:35:32 +0200

    11. Size

--- binary_trees.h
+++ binary_trees.h
@@ -81,4 +81,9 @@ size_t binary_tree_height(const binary_tree_t *tree);
  */
 size_t binary_tree_depth(const binary_tree_t *tree);
 
+/**
+ * measures the size of a binary tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree);
+
 #endif
